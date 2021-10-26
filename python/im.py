4f2a6d40ba452d6924e8971179872d91564866a0
JuanDaAC <jdaguirrec@unal.edu.co>
2021-10-26 08:36:19 -0500

    Comentario

--- python/im.py
+++ python/im.py
@@ -6,3 +6,5 @@ ejemplo = cv2.imread(r'C:\Users\User1\Pictures\Saved Pictures\patron.png',0)
 fft_ejemplo = np.fft.fftshift(np.fft.fftn(ejemplo))
 plt.imshow(np.log(np.abs(fft_ejemplo)))
 plt.show()
+
+# Este código hace una transformada de fourier a una imagen
